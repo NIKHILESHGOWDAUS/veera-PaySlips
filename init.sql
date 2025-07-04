4707eea431b44470eac5b599e377936044c16286
Nikhilesh <nikhileshgowda10@gmail.com>
2025-07-04 15:40:59 -0700

    commit2

--- init.sql
+++ init.sql
@@ -1,11 +1,18 @@
 
-       CREATE TABLE IF NOT EXISTS bonus_proposals (
-        id SERIAL PRIMARY KEY,
-        employee_name VARCHAR(100) NOT NULL,
-        employee_id VARCHAR(7) NOT NULL UNIQUE,
-        proposal_date DATE NOT NULL,
-        bonus_amount INTEGER NOT NULL,
-        reason TEXT NOT NULL,
-        CONSTRAINT valid_employee_id CHECK (employee_id ~ '^ATS0(?!000)[0-9]{3}$'),
-        CONSTRAINT valid_bonus_amount CHECK (bonus_amount >= 100)
-     );
+ CREATE TABLE IF NOT EXISTS payslips (
+                id SERIAL PRIMARY KEY,
+                employee_name VARCHAR(50) NOT NULL,
+                employee_id VARCHAR(7) NOT NULL,
+                department VARCHAR(30) NOT NULL,
+                employment_type VARCHAR(20) NOT NULL,
+                working_days INTEGER NOT NULL CHECK (working_days BETWEEN 1 AND 31),
+                date_of_joining DATE NOT NULL,
+                bank_details JSONB NOT NULL,
+                government_ids JSONB NOT NULL,
+                earnings JSONB NOT NULL,
+                deductions JSONB NOT NULL,
+                totals JSONB NOT NULL,
+                timestamp TIMESTAMPTZ NOT NULL,
+                CONSTRAINT unique_employee_month UNIQUE (employee_id, timestamp)
+            );
+
